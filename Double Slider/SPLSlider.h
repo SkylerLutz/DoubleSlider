3a0bf59ff3f201686921fe93a785bead0d71f567
Skyler Lutz <SkylerLutz@me.com>
2014-02-23 19:04:55 -0500

    Initial Commit

---
+++ Double Slider/SPLSlider.h
@@ -0,0 +1,13 @@
+//
+//  SPLSlider.h
+//  Double Slider
+//
+//  Created by Skyler Patrick Lutz on 2/23/14.
+//  Copyright (c) 2014 Skyler Patrick Lutz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPLSlider : UIControl
+
+@end
---
+++ Double Slider/SPLSlider.m
@@ -0,0 +1,31 @@
+//
+//  SPLSlider.m
+//  Double Slider
+//
+//  Created by Skyler Patrick Lutz on 2/23/14.
+//  Copyright (c) 2014 Skyler Patrick Lutz. All rights reserved.
+//
+
+#import "SPLSlider.h"
+
+@implementation SPLSlider
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
